8b6627232286e6d6152ca3cb10cc8d4ef724c84b
Darryl Mendonez <darryl.mendonez@nucleuscentral.com>
2019-04-09 13:18:36 -0400

    inverted the scales so bar graph looks normal

--- js/index.js
+++ js/index.js
@@ -7,48 +7,54 @@ const svg = d3.select('.canvas')
 // create margins and dimesions
 const margin = {top: 20, right: 20, bottom: 100, left: 100};
 const graphWidth = 600 - margin.left - margin.right;
-const graphHeight = 600 -margin.top - margin.bottom;
+const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg.append('g')
   .attr('width', graphWidth)
   .attr('height', graphHeight)
-  .attr('transform', `translate( ${margin.left},${margin.top})` )
+  .attr('transform', `translate( ${margin.left}, ${margin.top})` );
+
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`);
+
+const yAxisGroup = graph.append('g');
 
 d3.json('data/menu.json').then(data => {
 
   const y = d3.scaleLinear()
     .domain( [0, d3.max(data, d => d.orders) ] )
-    .range([0, 500]);
-
-  // const min = d3.min(data, d => d.orders);
-  // const max = d3.max(data, d => d.orders);
-  // const extent = d3.extent(data, d => d.orders);
-
-  // console.log(min);
-  // console.log(max);
-  // console.log(extent);
+    .range([graphHeight, 0]);
 
   const x = d3.scaleBand()
     .domain(data.map(item => item.name))
-    .range([0, 500])
+    .range([0, graphWidth])
     .paddingInner(0.2)
     .paddingOuter(0.2);
 
   // join the data to rects
-  const rects = svg.selectAll('rect')
+  const rects = graph.selectAll('rect')
     .data(data)
 
   rects.attr('width', x.bandwidth)
-    .attr('height', d => y( d.orders) )
+    .attr('height', d => graphHeight - y( d.orders) )
     .attr('fill', 'orange')
     .attr( 'x', d => x(d.name) )
+    .attr('y', d => y(d.orders));
 
   // append the enter selection to the DOM
   rects.enter()
     .append('rect')
     .attr('width', x.bandwidth)
-    .attr('height', d => y( d.orders) )
+    .attr('height', d => graphHeight - y( d.orders) )
     .attr('fill', 'orange')
-    .attr('x',  d => x(d.name) )
+    .attr('x', d => x(d.name) )
+    .attr('y', d => y(d.orders));
 
+    // create and call the axes
+    const xAxis = d3.axisBottom(x);
+    const yAxis = d3.axisLeft(y);
+
+    xAxisGroup.call(xAxis);
+    yAxisGroup.call(yAxis);
 })
